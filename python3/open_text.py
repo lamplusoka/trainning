6a21c75daec0d575608c339185420a00e4ba005f
lamplusoka <43769806+lamplusoka@users.noreply.github.com>
2018-10-28 17:40:19 +0900

    feat: added  function of reading writing text

---
+++ python3/open_text.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python3
+file_name = "/mnt/c/users/lamplus/documents/github/trainning/python3/test_library.txt"
+file = open(file_name)
+data = file.read()
+print(data)
---
+++ python3/write_text.py
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/python3
+file_name = "/mnt/c/users/lamplus/documents/github/trainning/python3/test_library.txt"
+file = open(file_name, 'w')
+file.write("dd")
+file.close()
+file = open(file_name)
+data = file.read()
+print(data)
